9dea21d040a7fd1e761566e37d84725c324952bd
silberringe <team@silberringe.de>
2017-08-22 15:04:04 +0200

    dark background

---
+++ src/Containers/SilberringeCeresThemeContainer.php
@@ -0,0 +1,13 @@
+    <?php
+     
+    namespace SilberringeCeresTheme\Containers;
+     
+    use Plenty\Plugin\Templates\Twig;
+     
+    class SilberringeCeresThemeContainer
+    {
+        public function call(Twig $twig):string
+        {
+            return $twig->render('SilberringeCeresTheme::content.SilberringeCeresTheme');
+        }
+    }
